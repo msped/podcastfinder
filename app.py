f4f47670a54ae9a0341312c8ba57d6209b3a58b6
Matt Edwards <mattsp.edwards@googlemail.com>
2023-02-03 21:49:04 +0000

    feat: added mongodb suuport

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask
 from dotenv import load_dotenv
+from flask_pymongo import PyMongo
 
 load_dotenv()
 
@@ -9,6 +10,8 @@ app = Flask(__name__)
 app.secret_key = os.getenv("SECERT_KEY")
 app.config["MONGO_DBNAME"] = os.getenv("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.getenv("MONGO_URI")
+
+mongo = PyMongo(app)
 
 if __name__ == "__main__":
     app.run(debug=True)
