e6097c8acb784be1b706369bb916648c01982803
Matt Edwards <mattsp.edwards@googlemail.com>
2023-02-06 19:55:11 +0000

    refactor: removed profanity check to go to v1 requirements

--- app.py
+++ app.py
@@ -14,10 +14,10 @@ app.config["MONGO_DBNAME"] = os.getenv("MONGO_DBNAME")
 mongo_client = pymongo.MongoClient(os.getenv("MONGO_URI"))
 db = mongo_client.get_database(os.getenv('MONGO_DBNAME'))
 
-def check_for_profanity_filter(transcript):
-    if "[\u00a0__\u00a0]" in transcript:
-        return True
-    return False
+# def check_for_profanity_filter(transcript):
+#     if "[\u00a0__\u00a0]" in transcript:
+#         return True
+#     return False
 
 def get_full_transcript_from_yt_api(transcript):
     text = []
@@ -37,19 +37,15 @@ def download_transcripts():
             video_transcript = YouTubeTranscriptApi.get_transcript(video_id)
             full_transcript = get_full_transcript_from_yt_api(video_transcript)
 
-            if check_for_profanity_filter(full_transcript):
-                # go to assembly ai for transcript
-                return 'Sent to third-party service for transcription', 200
-            else:
-                # save transcript to db
-                db.transcripts.insert_one({
-                    'video_id': video_id,
-                    'title': title,
-                    'channel': channel,
-                    'transcript_with_time': video_transcript,
-                    'full_transcript': full_transcript  
-                })
-                return 'Created', 201
+            # save transcript to db
+            db.transcripts.insert_one({
+                'video_id': video_id,
+                'title': title,
+                'channel': channel,
+                'transcript_with_time': video_transcript,
+                'full_transcript': full_transcript  
+            })
+            return 'Created', 201
         else:
             return 'Podcast already exists in database', 400
 
